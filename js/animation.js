0f8b2bc506135ad16668443b35cf80abdc631a47
renzorr <benaventer01@gmail.com>
2023-03-31 16:33:28 -0500

    change duration

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@ anime({
   targets: ".card",
   scale: [0, 1],
   opacity: [0, 1],
-  delay: 2200,
+  delay: 3200,
 });
 
 anime({
@@ -10,13 +10,13 @@ anime({
   innerHTML: [0, 76],
   round: 1,
   duration: 2000,
-  delay: 2200,
+  delay: 3200,
 });
 
 anime({
   targets: ".preloader",
   keyframes: [{ translateX: 2000 }, { display: "none" }],
-  duration: 3000,
+  duration: 5000,
   easing: "easeInQuad",
   delay: 90,
 });
